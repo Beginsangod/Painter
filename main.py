25f29ad689f16175738359af93141ec15e17ba1d
unknown <nlendcelestin638@gmail.com>
2025-11-07 22:37:19 +0100

    config du point d'entrée

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import PySide6.QtWidgets as QtW
 import PySide6.QtGui as QtG
 import PySide6.QtCore as QtC
 from Sources.Core.Gui.main_window import MainWindow
-
 import sys
 
 
-
-app = QtW.QApplication([])
-window = MainWindow()
-window.show()
-app.exec()
+if __name__ == "__main__":
+   app = QtW.QApplication(sys.argv)
+   window = MainWindow()
+   window.show()
+   sys.exit(app.exec())
